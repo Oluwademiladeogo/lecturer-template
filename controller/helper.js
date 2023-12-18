96513899c4a463e12087fa7703814fd23eecebab
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-12-18 13:27:44 +0100

    Feat: Improved error handling

--- controller/helper.js
+++ controller/helper.js
@@ -1,16 +1,24 @@
-require("dotenv").config()
+require("dotenv").config();
 function errorHandler(error, name, from) {
-    if (process.env.ENV==="DEV") {
-        
-    
-    let loggerFunction = console.log;
-    loggerFunction("-------START-------")
-    loggerFunction("Error occured in " + name)
-    if (from==="axios") {
-        
+  if (process.env.ENV === "DEV") {
+    console.log("-------START-------");
+    console.log("Error occured in " + name);
+    if (from === "axios") {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.toJSON());
+    } else {
+      console.log(error);
     }
-    loggerFunction("-------END-------")
-    }
+    console.log("-------END-------");
+  }
 }
 module.exports = {
   errorHandler,
