cfedbd988163ac2b0d2e95734331c6756517afbf
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-12-23 20:19:11 +0100

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const { handler } = require("./controller");
 const app = express();
-app.use(express.json())
-app.use("*", require("./routes/index"))
+app.use(express.json());
+
+app.post("*", async (req, res) => {
+  res.send(await handler(req, "POST"));
+});
+app.get("*", async (req, res) => {
+  res.send(await handler(req, "GET"));
+});
 module.exports = app;
